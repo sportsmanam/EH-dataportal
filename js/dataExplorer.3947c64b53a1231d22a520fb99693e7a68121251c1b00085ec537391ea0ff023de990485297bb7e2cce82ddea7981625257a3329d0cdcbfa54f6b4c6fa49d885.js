e89f9ef4d1915744f47c22c4adef8bb1259d22ae
cgettings <cgettings@users.noreply.github.com>
2022-11-14 21:56:52 +0000

    deploy: b98850c2b1798c081280ae37598ffb9e47e3b17a

--- js/dataExplorer.3947c64b53a1231d22a520fb99693e7a68121251c1b00085ec537391ea0ff023de990485297bb7e2cce82ddea7981625257a3329d0cdcbfa54f6b4c6fa49d885.js
+++ js/dataExplorer.3947c64b53a1231d22a520fb99693e7a68121251c1b00085ec537391ea0ff023de990485297bb7e2cce82ddea7981625257a3329d0cdcbfa54f6b4c6fa49d885.js
@@ -1179,6 +1179,9 @@ const renderMeasures = async () => {
 
     console.log("** renderMeasures");
 
+    selectedSummaryYears = [];
+    selectedSummaryGeography = [];
+
     linksMeasures.length = 0
 
     const contentSummary = document.querySelector('#tab-table');
@@ -1210,6 +1213,8 @@ const renderMeasures = async () => {
     // create years dropdown for table
 
     const tableYears = [...new Set(fullDataTableObjects.map(item => item.Time))];
+
+    console.log("tableYears", tableYears);
 
     tableYears.forEach((year, index) => {
 
